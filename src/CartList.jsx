39926894f2c43f9f192b38e77afbf72da4e70ffc
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-08-26 11:47:59 +0530

    done

--- src/CartList.jsx
+++ src/CartList.jsx
@@ -1,36 +1,56 @@
+import { useState } from "react";
 import { useSelector } from "react-redux"
 
 export default function CartList() {
     const cartSelector = useSelector((state) => state.cart.items)
     console.log(cartSelector);
+const [cartItems,setCartItems]=useState(cartSelector)
+    const manageQuantity=(id, q)=>{
+
+        let quantity= parseInt(q)>1?parseInt(q):1
+        const cartTempItems = cartSelector.map((item)=>{
+            return item.id==id?
+            {...item,quantity}:item
+        })
+        setCartItems(cartTempItems)
+
+
+    }
 
     return <>
         <div className="cart-container">
             <div className="cart-header">
                 <h2>Your Cart Items</h2>
-                <span>{cartSelector.length} items</span>
+                <span>{cartItems.length} items</span>
             </div>
             {
-                cartSelector.length > 0 ? cartSelector.map((item) => (
+                cartItems.length > 0 ? cartItems.map((item) => (
                     <div key={item.id} className="cart-item">
                         <div className="item-info">
                             <img src={item.thumbnail} />
                             <div className="item-details">
                                 <h4>{item.title}</h4>
                                 <p>{item.brand}</p>
+                            </div>
+                        </div>
+                        <div className="item-actions">
+                            <div style={{display:'flex'}}>
+                                <input onChange={(e)=>manageQuantity(item.id,e.target.value)} value={item.quantity?item.quantity:1}  style={{margin:'15px'}}  type="number" placeholder="enter quantity" />
+                                <div>
+                                    <span className="price">
+                                        ${(item.quantity?item.price*item.quantity:item.price).toFixed(2)}
+                                        </span>
+                                    <button className="btn">Remove</button>
                                 </div>
                             </div>
-                            <div className="item-actions"> 
-                                <span className="price">{item.price}</span>
-                                <button className="btn">Remove</button>
-                            </div>
+                        </div>
                     </div>
                 ))
                     : null
             }
 
             <div className="cart-footer">
-                Total : {cartSelector.reduce((sum,item)=>sum+item.price,0)}
+                Total : ${(cartItems.reduce((sum, item) => item.quantity? sum + item.price*item.quantity:sum+item.price , 0)).toFixed(2)}
             </div>
         </div>
     </>
