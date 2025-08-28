07bf394cbc5cbfd899a7be80c8b055ec18573388
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-08-28 19:17:01 +0530

    done

--- src/CartList.jsx
+++ src/CartList.jsx
@@ -1,18 +1,35 @@
-import { useState } from "react";
-import { useSelector } from "react-redux"
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux"
+import { clearAllItems, removeItem } from "./redux/slice";
+import { useNavigate } from "react-router-dom";
 
 export default function CartList() {
     const cartSelector = useSelector((state) => state.cart.items)
     console.log(cartSelector);
-const [cartItems,setCartItems]=useState(cartSelector)
-    const manageQuantity=(id, q)=>{
+    const [cartItems, setCartItems] = useState(cartSelector)
 
-        let quantity= parseInt(q)>1?parseInt(q):1
-        const cartTempItems = cartSelector.map((item)=>{
-            return item.id==id?
-            {...item,quantity}:item
+    useEffect(()=>{
+        setCartItems(cartSelector)
+    },[cartSelector])
+    const dispatch=useDispatch()
+    const navigate=useNavigate()
+    const manageQuantity = (id, q) => {
+
+        let quantity = parseInt(q) > 1 ? parseInt(q) : 1
+        const cartTempItems = cartSelector.map((item) => {
+            return item.id == id ?
+                { ...item, quantity } : item
         })
         setCartItems(cartTempItems)
+
+
+    }
+
+    const handlePlaceOrder=()=>{
+        localStorage.clear();
+        dispatch(clearAllItems());
+        alert("Order placed")
+        navigate("/")
 
 
     }
@@ -34,13 +51,13 @@ const [cartItems,setCartItems]=useState(cartSelector)
                             </div>
                         </div>
                         <div className="item-actions">
-                            <div style={{display:'flex'}}>
-                                <input onChange={(e)=>manageQuantity(item.id,e.target.value)} value={item.quantity?item.quantity:1}  style={{margin:'15px'}}  type="number" placeholder="enter quantity" />
+                            <div style={{ display: 'flex' }}>
+                                <input onChange={(e) => manageQuantity(item.id, e.target.value)} value={item.quantity ? item.quantity : 1} style={{ margin: '15px' }} type="number" placeholder="enter quantity" />
                                 <div>
                                     <span className="price">
-                                        ${(item.quantity?item.price*item.quantity:item.price).toFixed(2)}
-                                        </span>
-                                    <button className="btn">Remove</button>
+                                        ${(item.quantity ? item.price * item.quantity : item.price).toFixed(2)}
+                                    </span>
+                                    <button onClick={()=>dispatch(removeItem(item))} className="btn">Remove</button>
                                 </div>
                             </div>
                         </div>
@@ -50,8 +67,9 @@ const [cartItems,setCartItems]=useState(cartSelector)
             }
 
             <div className="cart-footer">
-                Total : ${(cartItems.reduce((sum, item) => item.quantity? sum + item.price*item.quantity:sum+item.price , 0)).toFixed(2)}
+                Total : ${(cartItems.reduce((sum, item) => item.quantity ? sum + item.price * item.quantity : sum + item.price, 0)).toFixed(2)}
             </div>
+            <button onClick={handlePlaceOrder} className="btn">Place Order</button>
         </div>
     </>
 
