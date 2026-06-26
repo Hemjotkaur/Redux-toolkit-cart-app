2104c6efc0fefa1834242c9e552bdc8651aea50b
Hemjotkaur <hemjotkaur786@gmail.com>
2026-06-26 09:20:59 +0530

    feat: display product listing

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,46 +1,29 @@
 import { useDispatch, useSelector } from "react-redux";
-import { addItem, removeItem } from "./redux/slice";
 import { useEffect } from "react";
 import { fetchProducts } from "./redux/productSlice";
 
 const Product = () => {
   const dispatch = useDispatch();
 
-  useEffect(()=>{
-    dispatch(fetchProducts())
-  },[])
-  const selector = useSelector((state)=>state.products)
-  console.log(selector);
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, []);
+  const productSelector = useSelector((state) => state.products.items);
   return (
-    <div className="product-card">
-      <div className="product-image h-[330px]">
-        <img
-          className="h-full w-full object-contain"
-          src="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aGVhZHBob25lc3xlbnwwfHwwfHx8MA%3D%3D"
-          alt="Wireless Headphones"
-        />
-      </div>
-
-      <div className="product-info">
-        <h1>Wireless Headphones</h1>
-
-        <p className="price">$129.99</p>
-
-        <p className="description">
-          Experience high-quality sound with these wireless headphones.
-          Featuring noise cancellation, long-lasting battery life, and a sleek
-          modern design for everyday use.
-        </p>
-
-        <button onClick={() => dispatch(addItem(1))} className="btn">
-          Add to Cart
-        </button>
-         <button onClick={() => dispatch(removeItem(1))} className="remove-button btn">
-          Remove from Cart
-        </button>
-      </div>
+    <div className="grid">
+      {productSelector.length &&
+        productSelector.map((item) => 
+        <div key={item} className="card">
+            <img src={item.thumbnail} />
+            <div className="title">{item.title}</div>
+            <div className="brand">{item.brand}</div>
+            <div className="price">{item.price}</div>
+            <div className="rating">{item.rating}</div>
+            <button className="btn">Add To Cart</button>
+        </div>)}
     </div>
   );
 };
 
 export default Product;
+ 
